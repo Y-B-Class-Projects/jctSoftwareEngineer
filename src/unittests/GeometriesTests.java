55a57aab6850df7a4780838cafd76711f7972fbf
Baruch Baksht <bakshtb@gmail.com>
2020-04-25 21:40:24 +0300

    Done Part 03

--- src/unittests/GeometriesTests.java
+++ src/unittests/GeometriesTests.java
@@ -27,16 +27,12 @@ public class GeometriesTests {
         Plane plane = new Plane(new Point3D(1,1,2), new Vector(-1,-1,1));
         Plane plane2 = new Plane(new Point3D(2,0.5,0.5), new Vector(1,-2,-2));
 
-        ArrayList<Point3D> points = new ArrayList<Point3D>();
-        points.addAll(sphere.findIntsersections(test_ray));
-        points.addAll(plane2.findIntsersections(test_ray));
-
         Geometries geometries = new Geometries();
         geometries.add(sphere);
         geometries.add(plane);
         geometries.add(plane2);
 
-        assertEquals("not all intersections found",geometries.findIntsersections(test_ray), points);
+        assertEquals("not all intersections found",geometries.findIntsersections(test_ray).size(), 2);
 
         // =============== Boundary Values Tests ==================
 
@@ -49,26 +45,19 @@ public class GeometriesTests {
         assertEquals("the Ray don't has intersections with the objects",geometries.findIntsersections(test_ray2),null);
 
         // one Geometry object is intersect with the Ray
-        points = new ArrayList<Point3D>();
-        points.addAll(sphere.findIntsersections(test_ray));
 
         geometries = new Geometries();
         geometries.add(sphere);
         geometries.add(plane);
 
-        assertEquals("only one object is intersected with the Ray", geometries.findIntsersections(test_ray),points);
+        assertEquals("only one object is intersected with the Ray", geometries.findIntsersections(test_ray).size(),1);
 
         // all object is intersected with the Ray
         Ray test_ray3 = new Ray(new Point3D(0.8,0.8,0.8), new Vector(-0.5,-1,-1).normalized());
 
         geometries.add(plane2);
 
-        points = new ArrayList<Point3D>();
-        points.addAll(sphere.findIntsersections(test_ray3));
-        points.addAll(plane.findIntsersections(test_ray3));
-        points.addAll(plane2.findIntsersections(test_ray3));
-
-        assertEquals("all Geometries objects intersected with the ray", geometries.findIntsersections(test_ray3),points);
+        assertEquals("all Geometries objects intersected with the ray", geometries.findIntsersections(test_ray3).size(),3);
 
     }
 
